970705f74569f963d621e1e55ce95bf18fe71f42
North Harrington <339713067@GoTVDSB.ca>
2024-12-12 09:25:28 -0500

    done

--- foolsmate/Game1.cs
+++ foolsmate/Game1.cs
@@ -13,7 +13,8 @@ namespace foolsmate
         Four,
         Five,
         Six,
-        Seven
+        Seven,
+        Eight,
     }
     public class Game1 : Game
     {
@@ -29,6 +30,7 @@ namespace foolsmate
         Texture2D bPawnTexture;
         Texture2D wQueenTexture;
         Texture2D wBishopTexture;
+        Texture2D blunderTexture;
 
 
 
@@ -37,11 +39,14 @@ namespace foolsmate
  
         Rectangle wBishoprect;
      
-        Rectangle bPawn1rect, bPawn2rect, bPawn3rect;
+        Rectangle bPawn1rect, bPawn2rect, bPawn3rect,bPawn4rect;
        
         Rectangle wQueenrect;
-       
-        
+
+        Rectangle blunder1rect, blunder2rect;
+
+
+        int moveSpeed;
       
         
         MouseState mouseState;
@@ -58,6 +63,7 @@ namespace foolsmate
 
             // TODO: Add your initialization logic here
             move = Move.One;
+            moveSpeed = 2;
             window = new Rectangle(0,0 ,600,600);
             wPawnrect = new Rectangle(300, 450, 70, 70);
             bPawn1rect = new Rectangle(300, 80, 70,70);
@@ -65,6 +71,10 @@ namespace foolsmate
             wQueenrect = new Rectangle(230, 540, 70, 70);
             bPawn2rect = new Rectangle(520, 90, 70, 70);
             bPawn3rect = new Rectangle(240, 90, 70, 70);
+            bPawn4rect = new Rectangle(381, 80, 70, 70);
+            blunder1rect = new Rectangle(200, 40, 1000, 1000);
+            blunder2rect = new Rectangle(400, 80, 1000, 1000);
+
  
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
@@ -83,6 +93,7 @@ namespace foolsmate
             bPawnTexture = Content.Load<Texture2D>("bPawn");
             wBishopTexture = Content.Load<Texture2D>("wBishop");
             wQueenTexture = Content.Load<Texture2D>("wQueen");
+            blunderTexture = Content.Load<Texture2D>("blunder (1)");
         }
 
         protected override void Update(GameTime gameTime)
@@ -94,27 +105,72 @@ namespace foolsmate
 
             if (move == Move.One)
             {
-                wPawnrect.Y -= 1;
+                wPawnrect.Y -= moveSpeed;
 
                 if (wPawnrect.Y <= 312)
                     move = Move.Two;
             }
             else if (move == Move.Two)
             {
-                bPawn1rect.Y += 1;
+                bPawn1rect.Y += moveSpeed;
                 if (bPawn1rect.Y >= 222)
                     move = Move.Three;
 
             }
             else if (move == Move.Three)
             {
-                wQueenrect.X += 1;
-                wQueenrect.Y -= 1;
+                wQueenrect.X += moveSpeed;
+                wQueenrect.Y -= moveSpeed;
                 if (wQueenrect.X >= 534)
                     move = Move.Four;
 
 
             }
+            else if (move== Move.Four)
+            {
+
+                bPawn2rect.Y += moveSpeed;
+                if (bPawn2rect.Y>= 160)
+                    move = Move.Five;
+            }
+            else if (move == Move.Five)
+            {
+
+                wBishoprect.X -= moveSpeed;
+                wBishoprect.Y -= moveSpeed;
+                if (wBishoprect.Y <= 305)
+                    move = Move.Six;
+            }
+            else if (move == Move.Six)
+            {
+
+                bPawn3rect.Y += moveSpeed;
+                if (bPawn3rect.Y >= 160)
+                    move = Move.Seven;
+            }
+            else if (move == Move.Seven)
+            {
+
+                wQueenrect.X -= moveSpeed;
+                wQueenrect.Y -= moveSpeed;
+
+                if (wQueenrect.Y <= 75)
+                {
+                    move = Move.Eight;
+                    bPawn4rect.Y = -100;
+                    
+
+
+                    
+                    
+                }
+            }
+
+
+
+
+
+
 
 
 
@@ -136,6 +192,9 @@ namespace foolsmate
             _spriteBatch.Draw(bPawnTexture, bPawn1rect, Color.Green);
             _spriteBatch.Draw(bPawnTexture, bPawn2rect, Color.White);
             _spriteBatch.Draw(bPawnTexture, bPawn3rect, Color.Red);
+            _spriteBatch.Draw(bPawnTexture,bPawn4rect, Color.White);
+            _spriteBatch.Draw(blunderTexture, blunder1rect, Color.White);
+            _spriteBatch.Draw(bPawnTexture, blunder2rect, Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
